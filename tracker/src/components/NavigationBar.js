ddaddf6af5dfee52057521e27ca78638128b59cb
JMJ Riyana <jeemariyana@gmail.com>
2020-11-19 00:19:26 +0530

    init

--- tracker/src/components/NavigationBar.js
+++ tracker/src/components/NavigationBar.js
@@ -6,10 +6,10 @@ export default function NavigationBar() {
     return (
         
         <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#">Home</Navbar.Brand>
+            <Navbar.Brand href="/Home">Home</Navbar.Brand>
                 <Nav className="mr-auto">
-                <Nav.Link href="#">Login</Nav.Link>
-                <Nav.Link href="#">Signup</Nav.Link>
+                <Nav.Link href="./Login">Login</Nav.Link>
+                <Nav.Link href="./Signup">Signup</Nav.Link>
             </Nav>
         </Navbar>
     );
