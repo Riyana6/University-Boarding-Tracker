27e121bb7a018c396080ddcb56f44514b3add01c
JMJ Riyana <jeemariyana@gmail.com>
2020-11-19 19:02:56 +0530

    update

--- tracker/src/components/NavigationBar.js
+++ tracker/src/components/NavigationBar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 export default function NavigationBar() {
 
     return (
         
         <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="/Home">Home</Navbar.Brand>
-                <Nav className="mr-auto">
-                <Nav.Link href="./Login">Login</Nav.Link>
-                <Nav.Link href="./Signup">Signup</Nav.Link>
+            <Navbar.Brand href="/">Home</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Link to={"Login"} className="nav-link">Login</Link>
+                <Link to={"Signup"} className="nav-link">Signup</Link>
             </Nav>
         </Navbar>
     );
