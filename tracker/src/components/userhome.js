d27c76d891454df31d5c5be2cd07ab5f0d2fc5b0
JMJ Riyana <jeemariyana@gmail.com>
2021-02-14 09:56:04 +0530

    updated

--- tracker/src/components/userhome.js
+++ tracker/src/components/userhome.js
@@ -22,6 +22,29 @@ export default class UserHome extends Component {
                         <input type="text" id="defaultFormRegisterNameEx" className="form-control" />
                         <br />
                         <label htmlFor="defaultFormRegisterEmailEx" className="grey-text">
+                            Select the nearest university
+                        </label>
+                        <select id = "dropdown">
+                                        <option value="N/A">Select your univeristy</option>
+                                        <option value="1">University of Colombo</option>
+                                        <option value="2">University of Moratuwa</option>
+                                        <option value="3">University of Sri Jayewardenepura</option>
+                                        <option value="4">University of Kelaniya</option>
+                                        <option value="5">University of Peradeniya</option>
+                                        <option value="6">University of Ruhuna</option>
+                                        <option value="7">University of Jaffna</option>
+                                        <option value="8">South Eastern University of Sri Lanka</option>
+                                        <option value="9">Rajarata University of Sri Lanka</option>
+                                        <option value="10">Wayamba University of Sri Lanka</option>
+                                        <option value="11">Uva Wellassa University</option>
+                                        <option value="12">Sabaragamuwa University of Sri Lanka</option>
+                                        <option value="13">Eastern University of Sri Lanka</option>
+                                        <option value="14">University of the Visual and Performing Arts</option>
+                                        <option value="15">Buddhist and Pali University</option>
+                                        <option value="16">Bhiksu University of Sri Lanka</option>
+                                    </select>
+                        <br />
+                        <label htmlFor="defaultFormRegisterEmailEx" className="grey-text">
                             Address
                         </label>
                         <input type="email" id="defaultFormRegisterEmailEx" className="form-control" />
@@ -48,7 +71,7 @@ export default class UserHome extends Component {
                         <label htmlFor="defaultFormRegisterPasswordEx" className="grey-text">
                             Additional Details
                         </label>
-                        <input type="text" id="defaultFormRegisterPasswordEx" className="form-control" />
+                        <textarea rows="4" cols="100" name="skills"></textarea>
                         <div className="text-center mt-4">
                             
                             <MDBBtn color="primary" type="submit">
